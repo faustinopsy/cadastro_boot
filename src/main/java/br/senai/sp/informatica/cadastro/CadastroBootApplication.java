45ae5bff645bbe2d83c03185e0fa3acd105f6f08
Rodrigo Faustino <ev.rodrigo.eterno@gmail.com>
2019-08-26 20:00:51 -0300

    Initial commit

---
+++ src/main/java/br/senai/sp/informatica/cadastro/CadastroBootApplication.java
@@ -0,0 +1,13 @@
+package br.senai.sp.informatica.cadastro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastroBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadastroBootApplication.class, args);
+	}
+
+}
